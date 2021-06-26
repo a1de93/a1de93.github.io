550091850a22c3d63ad00cd8bedfb2aaa8c25b86
a1de93 <41848413+a1de93@users.noreply.github.com>
2021-06-26 01:46:19 -0500

    slower globe rotation & crop fix

--- globe-js/globe.js
+++ globe-js/globe.js
@@ -10,7 +10,7 @@ globe = new ENCOM.Globe(
     pinColor: "#39FF14",
     satelliteColor: "#39FF14",
     scale: 1.0,
-    dayLength: 30000,
+    dayLength: 45000,
     introLinesDuration: 2000,
     maxPins: 500,
     maxMarkers: 4,
