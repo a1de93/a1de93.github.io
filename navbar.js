62f0b38a2154fa87b9de758f8755fb3afdbe854c
a1de93 <41848413+a1de93@users.noreply.github.com>
2023-12-20 15:27:45 -0600

    ui changes
    image background clouds using flex, navbar button redesign, footer-link is now bottom right, minor mobile changes

--- navbar.js
+++ navbar.js
@@ -1,8 +1,7 @@
 //navbar
 const btn = document.querySelector(".btn");
-        
 btn.addEventListener("click",()=>{
-    document.body.classList.toggle("active")
+    document.body.classList.toggle("active");
 });
 
 //loading animation screen
@@ -14,5 +13,66 @@ window.addEventListener("load", () => {
         loader.addEventListener("transitioned", () => {
             document.body.removeChild("loader");
         })
-    }, 2700);
+    }, 2000);
 })
+
+//masonry grid for portfolio images
+console.clear();
+const elApp = document.querySelector("#app");
+const elImages = Array.from(document.querySelectorAll(".gallery-image"));
+const elDetail = document.querySelector(".detail");
+
+function flipImages(firstEl, lastEl, change) {
+  const firstRect = firstEl.getBoundingClientRect();
+  const lastRect = lastEl.getBoundingClientRect();
+
+  // INVERT
+  const deltaX = firstRect.left - lastRect.left;
+  const deltaY = firstRect.top - lastRect.top;
+  const deltaW = firstRect.width / lastRect.width;
+  const deltaH = firstRect.height / lastRect.height;
+
+  change();
+  lastEl.parentElement.dataset.flipping = true;
+
+  const animation = lastEl.animate([
+    {
+      transform: `none` //translateX(${deltaX}px) translateY(${deltaY}px) scaleX(${deltaW}) scaleY(${deltaH})
+    },
+    {
+      transform: 'none'
+    }
+  ], {
+    duration: 600, // milliseconds
+    easing: 'cubic-bezier(.2, 0, .3, 1)'
+  });
+
+  animation.onfinish = () => {
+    delete lastEl.parentElement.dataset.flipping;
+  }
+}
+
+elImages.forEach(figure => {
+  figure.addEventListener("click", () => {
+    const elImage = figure.querySelector('img');
+
+    elDetail.innerHTML = "";
+
+    const elClone = figure.cloneNode(true);
+    elDetail.appendChild(elClone);
+
+    const elCloneImage = elClone.querySelector('img');
+
+    flipImages(elImage, elCloneImage, ()=>{
+      elApp.dataset.state="detail";
+    });
+
+    function revert(){
+      flipImages(elCloneImage, elImage, ()=>{
+        elApp.dataset.state="gallery";
+        elDetail.removeEventListener('click',revert);
+      });
+    }
+    elDetail.addEventListener('click',revert);
+  });
+});
